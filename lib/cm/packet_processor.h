b3749df89829cbcd5da1f264cbbbe80daefea5da
cfdream <lxuemei3000@gmail.com>
2015-08-17 16:30:38 +0000

    add sample at switch

--- lib/cm/packet_processor.h
+++ lib/cm/packet_processor.h
@@ -13,7 +13,9 @@
 
 void cm_task_init(void);
 
-int packet_sampled(struct eth_header *eh);
+int packet_sampled(struct eth_header* eh, packet_t* p_packet, struct drand48_data* p_rand_buffer, int switch_id);
+int switch_packet_sampled(packet_t* p_packet, struct drand48_data* p_rand_buffer, int switch_id);
+int host_packet_sampled(struct eth_header *eh);
 //return 0: packet not dropped, 1:packet dropped
 int process(const struct dp_packet *packet, const struct dpif* dpif, struct drand48_data* p_rand_buffer);
 void process_normal_packet(int switch_id, packet_t* packet);
