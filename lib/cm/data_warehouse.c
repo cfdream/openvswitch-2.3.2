764323a4c5f6e544a8edaf7982c69f1921489524
cfdream <lxuemei3000@gmail.com>
2015-09-11 04:23:13 +0000

    add uniform_mem_ratio_to_diverse_mem

--- lib/cm/data_warehouse.c
+++ lib/cm/data_warehouse.c
@@ -19,15 +19,12 @@ int data_warehouse_init(void) {
 
     int max_switch_map_size = 0;
     if (cm_experiment_setting.switch_mem_type == UNIFORM) {
-        uint64_t max_switch_interval_volume = 0;
-        for (switch_idx = 0; switch_idx < NUM_SWITCHES; ++switch_idx) {
-            if (cm_experiment_setting.sample_hold_setting.switches_interval_volume[switch_idx] > max_switch_interval_volume) {
-                max_switch_interval_volume = cm_experiment_setting.sample_hold_setting.switches_interval_volume[switch_idx];
-            }
-        } 
-        max_switch_map_size = (int)(cm_experiment_setting.switch_memory_times
+        max_switch_map_size = (int)(
+                                    cm_experiment_setting.switch_memory_times
                                     * cm_experiment_setting.sample_hold_setting.default_byte_sampling_rate
-                                    * max_switch_interval_volume) ;
+                                    * cm_experiment_setting.sample_hold_setting.max_switch_interval_volume
+                                    * cm_experiment_setting.sample_hold_setting.uniform_mem_ratio_to_diverse_mem
+                                    ) ;
         if (max_switch_map_size <= 0) {
             ERROR("FAIL: max_switch_map_size=0");
             return -1;
